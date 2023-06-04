7690950bdcce21e61cc4fc887c7e8f63afb7aba0
Rekin Barışıcı <rekinbar2000@gmail.com>
2023-06-04 10:40:39 +0300

    refactor and adjustments

--- Assets/_Code/AntBase/AntMovement.cs
+++ Assets/_Code/AntBase/AntMovement.cs
@@ -6,9 +6,7 @@ namespace AntBase
 {
     public class AntMovement : MonoBehaviour
     {
-        public float speed = 2;
-        public float rotationSpeed = 2;
-
+        public AntSettings antSettings;
 
         private Vector2 _position;
         private Vector2 _velocity;
@@ -33,59 +31,23 @@ namespace AntBase
         
         private void RandomWalk()
         {
-            // if (_isRotating)
-            // {
-            //     Debug.DrawRay(transform.position, _randomForward, Color.red);
-            //     var right = transform.right;
-            //     var differenceX = Math.Abs(right.normalized.x - _randomForward.normalized.x);
-            //     var differenceY = Math.Abs(right.normalized.y - _randomForward.normalized.y);
-            //
-            //     if (differenceX < 0.02f && differenceY < 0.02f)
-            //     {
-            //         _isRotating = false;
-            //         _isMoving = true;
-            //     }
-            //     else
-            //     {
-            //         Debug.Log(transform.right+" "+_randomForward);
-            //         transform.right = Vector2.Lerp(transform.right, _randomForward, rotationSpeed);
-            //     }
-            // }
-            // else
-            // {
-            //     _randomForward = (UnityEngine.Random.insideUnitSphere.normalized).normalized;
-            //     _isRotating = true;
-            // }
-            //
-            // if(_isMoving)
-            // {
-            //     var newPos = transform.position + transform.right * speed;
-            //     transform.position = newPos;
-            // }
-            
-            
             if(!_isRotating)
             {
                 _isRotating = true;
                 
                 var angle = new Vector3(0,0,Vector2.SignedAngle(transform.right, _randomForward));
                 
-                transform.DORotate(new Vector3(angle.x, angle.y, angle.z), rotationSpeed).SetSpeedBased().SetEase(Ease.InOutSine).OnComplete(() =>
+                transform.DORotate(new Vector3(angle.x, angle.y, angle.z), antSettings.rotationSpeed).SetSpeedBased().SetEase(Ease.InOutSine).OnComplete(() =>
                 {
                     _isRotating = false;
                     _isMoving = true;
                     _randomForward = (UnityEngine.Random.insideUnitSphere.normalized).normalized;
-                    Debug.Log("Rotation complete");
                 });
             }
-            else
-            {
-                
-            }
-            
+
             if(_isMoving)
             {
-                var newPos = transform.position + transform.right * speed;
+                var newPos = transform.position + transform.right * antSettings.speed;
                 transform.position = newPos;
             }
         }
@@ -93,12 +55,10 @@ namespace AntBase
 
         public void OnObstacleDetected()
         {
-            // DOTween.Kill(transform);
-            // _isRotating = false;
-            // _isMoving = false;
-            // _randomForward = -transform.right;
-            // Debug.Log("Obstacle detected");
-            
+            DOTween.Kill(transform);
+            _isRotating = false;
+            _isMoving = false;
+            _randomForward = -transform.right;
         }
     }
 }
---
+++ Assets/_Code/AntBase/AntSettings.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace AntBase
+{
+    [CreateAssetMenu(fileName = "AntSettings", menuName = "AntSettings", order = 0)]
+    public class AntSettings : ScriptableObject
+    {
+        [Header("Movement")]
+        public float speed = 2;
+        public float rotationSpeed = 2;
+        
+        
+    }
+}
